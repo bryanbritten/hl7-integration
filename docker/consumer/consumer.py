6001a4c23e16b86c8d44fc607bb28eb5c22e2574
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-07-20 15:56:45 -0400

    connects the consumer service to the minio service

--- docker/consumer/consumer.py
+++ docker/consumer/consumer.py
@@ -1,15 +1,31 @@
 import asyncio
+import boto3
+from botocore.config import Config
 import os
 
 from asyncio import StreamReader, StreamWriter
 from datetime import datetime, timezone
 
+# In a production environment, these values would be in a .env file
 PORT = 2575
-OUTPUT_DIR = "/messages"
+MINIO_ENDPOINT = "minio:9000"
+MINIO_ACCESS_KEY = "admin"
+MINIO_SECRET_KEY = "password123"
+MINIO_BUCKET = "bronze"
 
 START = b"\x0b"
 END = b"\x1c"
 CR = b"\x0d"
+
+# Initialize fake S3 bucket
+s3 = boto3.client(
+    "s3",
+    endpoint_url=f"http://{MINIO_ENDPOINT}",
+    aws_access_key_id=MINIO_ACCESS_KEY,
+    aws_secret_access_key=MINIO_SECRET_KEY,
+    config=Config(signature_version="s3v4"),
+    region_name="us-east-1",
+)
 
 
 def build_ack() -> bytes:
@@ -18,14 +34,27 @@ def build_ack() -> bytes:
     return START + ack.encode("utf-8") + END + CR
 
 
+def store_message(content: str) -> None:
+    """
+    Uploads an HL7 message to MinIO with a timestamped key.
+    """
+    timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S%f")
+    object_name = f"adt/a01/{timestamp}.hl7"
+    try:
+        s3.put_object(
+            Bucket=MINIO_BUCKET, Key=object_name, Body=content.encode("utf-8")
+        )
+
+    # In production, specific Exceptions should be used for known or foreseeable issues
+    except Exception as e:
+        print(f"Error storing message: {e}")
+
+
 async def handle_client(reader: StreamReader, writer: StreamWriter) -> None:
     try:
         data = await reader.readuntil(END + CR)
         raw_message = data.strip(END + CR).strip(START).decode()
-        timestamp = datetime.now(timezone.utc).isoformat()
-        file_path = os.path.join(OUTPUT_DIR, f"msg_{timestamp}.hl7")
-        with open(file_path, "w") as f:
-            f.write(raw_message)
+        store_message(raw_message)
         writer.write(build_ack())
         await writer.drain()
         writer.close()
