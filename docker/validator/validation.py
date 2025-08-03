7bf5569027486e679b9ce29b8fe0cf91ee27da76
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-03 19:18:01 -0400

    updates validation logic to allow checking validity of all message types

--- docker/validator/validation.py
+++ docker/validator/validation.py
@@ -1,5 +1,6 @@
 import logging
 
+from hl7_helpers import MESSAGE_REGISTRY
 from hl7apy.core import Message
 from hl7apy.exceptions import ParserError
 from hl7apy.parser import parse_message
@@ -14,10 +15,12 @@ logger = logging.getLogger(__name__)
 class HL7Validator:
     def __init__(self, message: bytes):
         self.raw_message = message.decode("utf-8")
-        self.parsed_message = HL7Validator._parse(message.decode("utf-8"))
+        self.parsed_message = HL7Validator.__parse(message.decode("utf-8"))
+        self.message_type = self.parsed_message.msh.msh_9.msh_9_3.to_er7()
+        self.message_schema = MESSAGE_REGISTRY.get(self.message_type)
 
     @staticmethod
-    def _parse(raw_message) -> Message:
+    def __parse(raw_message) -> Message:
         """
         Parses the raw HL7 message if valid.
 
@@ -31,129 +34,87 @@ class HL7Validator:
             logger.error(f"Failed to parse message: {str(e)}")
             return Message("ADT_A01")
 
-    def has_msh_segment(self) -> bool:
+    def has_required_segments(self):
         """
-        Checks if the HL7 message contains an MSH segment.
+        Iterates through the required segments of an HL7 message, as defined by the message
+        registry, and confirms that segment is present in the message.
 
-        returns: bool - True if MSH segment is present, False otherwise.
+        Returns: bool - True if all required segments are present, else False.
         """
-        return self.parsed_message.msh is not None
-
-    def has_pid_segment(self) -> bool:
-        """
-        Checks if the HL7 message contains an PID segment.
-
-        returns: bool - True if PID segment is present, False otherwise.
-        """
-        return self.parsed_message.pid is not None
-
-    def has_evn_segment(self) -> bool:
-        """
-        Checks if the HL7 message contains an EVN segment.
-
-        returns: bool - True if EVN segment is present, False otherwise.
-        """
-        return self.parsed_message.evn is not None
-
-    def has_pv1_segment(self) -> bool:
-        """
-        Checks if the HL7 message contains a PV1 segment.
-
-        returns: bool - True if PV1 segment is present, False otherwise.
-        """
-        return self.parsed_message.pv1 is not None
-
-    def msh_segment_is_valid(self) -> bool:
-        """
-        Validates the MSH segment of the HL7 message.
-
-        returns: bool - True if MSH segment is valid, False otherwise.
-        """
-        if not self.has_msh_segment():
+        if self.message_schema is None:
+            logger.error(f"Unrecognized message type: {self.message_type}")
             return False
 
-        msh = self.parsed_message.msh
+        required_segments = [
+            segment["identifier"]
+            for segment in self.message_schema["segments"]
+            if segment["required"]
+        ]
 
-        if not msh.msh_2 or len(msh.msh_2.value) < 4:
-            return False
+        return all(
+            [
+                self.parsed_message.children.get(required_segment)
+                for required_segment in required_segments
+            ]
+        )
 
-        if (
-            not msh.msh_7
-            or not msh.msh_9
-            or not msh.msh_10
-            or not msh.msh_11
-            or not msh.msh_12
-        ):
-            return False
+    def all_segments_are_valid(self):
+        """
+        Iterates through all the segments in a given HL7 message and checks the message
+        registry to determine if that segment should be present in the given message type.
+
+        NOTE: Because the hl7apy library automatically groups segments based on the segment
+        and the message type, segments like IN1 in an ADT_A01 message will automatically be
+        grouped into a group named after the message type and segment group (e.g.
+        ADT_A01_INSURANCE), which maps directly to how the segment is named in the message
+        registry. Thus, the assumption is that if a segment like "ADT_A01_INSURANCE" is in
+        the segment names, then the segment is valid, without the need to specifically check
+        each underlying segment that make up the segment group.
+
+        Returns: bool - True if all segments are valid segments as defined by the HL7 standard,
+        else False.
+        """
+        valid_segments = [
+            segment["identifier"]
+            for segment in self.message_schema["segments"]
+            if segment.get("segments") is None
+        ]
+        return all(
+            [child.name in valid_segments for child in self.parsed_message.children]
+        )
+
+    def segment_cardinality_is_valid(self):
+        """
+        Iterates through all the segments in a given HL7 message and ensures each segment is
+        present only once, unless the segment can be repeated.
+
+        NOTE: HL7v2.5 ADT_A01 and ADT_A03 only contain segments that are not repeatable or are
+        repeatable an infinite number of times. As a result, this function does not bother
+        counting the number of occurrences, but a production-level integration engine would.
+
+        NOTE: For simplicity, cardinality of segment groups is ignored, but a production-level
+        integration engine would need to check to ensure that, for example, an IN2 segment is
+        matched with an IN1 segment.
+
+        Returns: bool - True if all segments are present one time unless they can be repeated,
+        else False.
+        """
+        seen = set()
+        repeatable_segments = [
+            segment["identifier"]
+            for segment in self.message_schema["segments"]
+            if segment["repeatable"] == True
+        ]
+        for child in self.parsed_message.children:
+            if child.name in seen and child.name not in repeatable_segments:
+                return False
+            seen.add(child.name)
 
         return True
 
-    def pid_segment_is_valid(self) -> bool:
-        """
-        Validates the PID segment of the HL7 message.
-
-        returns: bool - True if PID segment is valid, False otherwise.
-        """
-        if not self.has_pid_segment():
-            return False
-
-        pid = self.parsed_message.pid
-        if not pid.pid_3 or not pid.pid_5:
-            return False
-
-        return True
-
-    def evn_segment_is_valid(self) -> bool:
-        """
-        Validates the EVN segment of the HL7 message.
-
-        returns: bool - True if EVN segment is valid, False otherwise.
-        """
-        if not self.has_evn_segment():
-            return False
-
-        evn = self.parsed_message.evn
-        if not evn.evn_1 or not evn.evn_2:
-            return False
-
-        return True
-
-    def pv1_segment_is_valid(self) -> bool:
-        """
-        Validates the PV1 segment of the HL7 message.
-
-        returns: bool - True if PV1 segment is valid, False otherwise.
-        """
-        if not self.has_pv1_segment():
-            return False
-
-        pv1 = self.parsed_message.pv1
-        if not pv1.pv1_2 or len(pv1.pv1_2.value) > 1:
-            return False
-
-        return True
-
-    def message_is_valid(self) -> bool:
-        """
-        Validates the entire HL7 message.
-
-        returns: bool - True if the message is valid, False otherwise.
-        """
-
-        if not self.has_msh_segment():
-            return False
-        if not self.msh_segment_is_valid():
-            return False
-        if not self.has_pid_segment():
-            return False
-        if not self.pid_segment_is_valid():
-            return False
-        if not self.has_evn_segment():
-            return False
-        if not self.evn_segment_is_valid():
-            return False
-        if not self.has_pv1_segment():
-            return False
-        if not self.pv1_segment_is_valid():
-            return False
-        return True
+    def message_is_valid(self):
+        return (
+            self.has_required_segments()
+            and self.all_segments_are_valid()
+            and self.segment_cardinality_is_valid()
+        )
