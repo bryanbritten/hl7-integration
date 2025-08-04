d07159cdc9b98b14b93cbd80e7e03816ae84e838
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-03 21:43:15 -0400

    adds a manual msh segment extractor on consumption

--- docker/consumer/consumer.py
+++ docker/consumer/consumer.py
@@ -4,6 +4,7 @@ from asyncio import StreamReader, StreamWriter
 from datetime import datetime, timezone
 
 from hl7_helpers import get_msh_segment
+from hl7apy.exceptions import ParserError
 from prometheus_client import Counter, start_http_server
 from s3_helpers import MINIO_BRONZE_BUCKET, MINIO_DEADLETTER_BUCKET, write_data_to_s3
 
@@ -42,6 +43,14 @@ def build_ack(
     return START + msh_segment.encode("utf-8") + CR + msa_segment + END + CR
 
 
+def manually_extract_msh_segment(message: str) -> str:
+    segments = message.split("\r")
+    msh_candidates = [segment for segment in segments if segment.startswith("MSH")]
+    if not any(msh_candidates):
+        raise ValueError(f"No MSH segment present: {segments}")
+    return msh_candidates[0]
+
+
 async def handle_client(reader: StreamReader, writer: StreamWriter) -> None:
     # instantiate variables to be used in exception handling
     timestamp = None
@@ -55,11 +64,24 @@ async def handle_client(reader: StreamReader, writer: StreamWriter) -> None:
         timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S%f")
         logger.info("Message received")
 
-        msh_segment = get_msh_segment(message)
-        separator = msh_segment.msh_1.to_er7()
-        message_type = msh_segment.msh_9.msh_9_1.to_er7()
-        trigger_event = msh_segment.msh_9.msh_9_2.to_er7()
-        message_control_id = msh_segment.msh_10.to_er7()
+        try:
+            msh_segment = get_msh_segment(message.decode("utf-8"))
+            separator = msh_segment.msh_1.to_er7()
+            message_type = msh_segment.msh_9.msh_9_1.to_er7()
+            trigger_event = msh_segment.msh_9.msh_9_2.to_er7()
+            message_control_id = msh_segment.msh_10.to_er7()
+        except ParserError:
+            logger.warning(
+                "Failed to automatically extract MSH segment, so doing it manually"
+            )
+            msh_segment = manually_extract_msh_segment(message.decode("utf-8"))
+            separator = msh_segment[3]
+            fields = msh_segment.split(separator)
+            message_type, trigger_event, _ = fields[8].split("^")
+            message_control_id = fields[9]
+        if not isinstance(msh_segment, str):
+            msh_segment = msh_segment.to_er7()
+
         messages_received_total.labels(message_type=message_type).inc()
 
         key = f"unprocessed/{message_type}/{trigger_event}/{timestamp}.hl7"
@@ -70,7 +92,7 @@ async def handle_client(reader: StreamReader, writer: StreamWriter) -> None:
         )
 
         ack = build_ack(
-            msh_segment=msh_segment.to_er7(),
+            msh_segment=msh_segment,
             separator=separator,
             acknowledgement_code="AA",
             message_control_id=message_control_id,
@@ -80,24 +102,23 @@ async def handle_client(reader: StreamReader, writer: StreamWriter) -> None:
         writer.close()
     # In production, specific Exceptions should be used for known or foreseeable issues
     except Exception as e:
+        if timestamp is None:
+            timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S%f")
+
         # if there is no message then there was an error receiving the message; no need for ACK
         if message is None:
             logger.exception(f"Failed to ingest message: {str(e)}")
         # if the message_control_id is None then the message either failed to parse
         # or had a malformed MSH header. Either way, no way to automatically send ACK.
         elif message_control_id is None:
-            if timestamp is None:
-                timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S%f")
-            write_data_to_s3(
-                bucket=MINIO_DEADLETTER_BUCKET,
-                key=f"unparsed/{timestamp}.hl7",
-                body=message,
-            )
+            logger.exception(f"Failed to parse the MSH header: {str(e)}")
+        # if the message was received and the MSH header was parsed, then something
+        # unidentified occurred.
         elif msh_segment is not None:
             logger.exception(f"Error handling client: {str(e)}")
             ack = build_ack(
-                msh_segment=msh_segment.to_er7(),
-                separator=msh_segment.msh_1.to_er7(),
+                msh_segment=msh_segment,
+                separator=separator,  # type: ignore
                 acknowledgement_code="AE",
                 message_control_id=message_control_id,
             )
@@ -106,6 +127,11 @@ async def handle_client(reader: StreamReader, writer: StreamWriter) -> None:
             writer.close()
         else:
             logger.exception(f"Unexpected error: {str(e)}")
+        write_data_to_s3(
+            bucket=MINIO_DEADLETTER_BUCKET,
+            key=f"unparsed/{timestamp}.hl7",
+            body=message,
+        )
 
 
 async def main() -> None:
