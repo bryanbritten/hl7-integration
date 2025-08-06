fa765dd818f44fd663ec5f11c2b49f2484f046bc
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-06 10:55:52 -0400

    removes redundant variable assigment in segment generation functions

--- docker/producer/hl7_segment_generators.py
+++ docker/producer/hl7_segment_generators.py
@@ -36,40 +36,29 @@ def generate_msh_segment(message_type: str) -> bytes:
     """
     Generates an MSH segment for HL7 messages.
 
-    message_type: str - The type of HL7 message (e.g., "ADT^A01").
+    message_type: str - The type of HL7 message (e.g., "ADT^A01^ADT_A01").
     Returns: bytes - The MSH segment encoded in UTF-8.
     """
 
-    msh_0 = "MSH"
-    msh_1 = SEPARATOR
-    msh_2 = DELIMITERS
-    msh_3 = "PythonHL7Producer"
-    msh_4 = "Bryan's Lab"
-    msh_5 = "PythonHL7Consumer"
-    msh_6 = "Bryan's Ingestion Company"
-    msh_7 = generate_random_date_time()
-    msh_8 = ""
-    msh_9 = message_type
-    msh_10 = faker.uuid4()
-    msh_11 = "T"
-    msh_12 = "2.5"
-
-    return msh_1.join(
+    # fmt: off
+    return SEPARATOR.join(
         [
-            msh_0,
-            msh_2,
-            msh_3,
-            msh_4,
-            msh_5,
-            msh_6,
-            msh_7,
-            msh_8,
-            msh_9,
-            msh_10,
-            msh_11,
-            msh_12,
+            "MSH",                          # MSH-0
+            SEPARATOR,                      # MSH-1
+            DELIMITERS,                     # MSH-2
+            "PythonHL7Producer",            # MSH-3
+            "Bryan's Lab",                  # MSH-4
+            "PythonHL7Consumer",            # MSH-5
+            "Bryan's Ingestion Company",    # MSH-6
+            generate_random_date_time(),    # MSH-7
+            "",                             # MSH-8
+            message_type,                   # MSH-9
+            faker.uuid4(),                  # MSH-10
+            "T",                            # MSH-11
+            "2.5",                          # MSH-12
         ]
     ).encode("utf-8")
+    # fmt: on
 
 
 def generate_pid_segment() -> bytes:
@@ -79,51 +68,32 @@ def generate_pid_segment() -> bytes:
     Returns: bytes - The PID segment encoded in UTF-8.
     """
 
-    pid_0 = "PID"
-    pid_1 = "1"
-    pid_2 = ""
-    pid_3 = faker.uuid4()
-    pid_4 = ""
-    pid_5 = generate_patient_name()
-    pid_6 = faker.last_name()
-    pid_7 = generate_patient_birth_date()
-    pid_8 = generate_gender()
-    pid_9 = ""
-    pid_10 = generate_patient_race()
-    pid_11 = generate_patient_address()
-    pid_12 = ""
-    pid_13 = generate_patient_phone_number()
-    pid_14 = ""
-    pid_15 = ""
-    pid_16 = generate_patient_marital_status()
-    pid_17 = ""
-    pid_18 = ""
-    pid_19 = generate_patient_ssn()
-
+    # fmt: off
     return SEPARATOR.join(
         [
-            pid_0,
-            pid_1,
-            pid_2,
-            pid_3,
-            pid_4,
-            pid_5,
-            pid_6,
-            pid_7,
-            pid_8,
-            pid_9,
-            pid_10,
-            pid_11,
-            pid_12,
-            pid_13,
-            pid_14,
-            pid_15,
-            pid_16,
-            pid_17,
-            pid_18,
-            pid_19,
+            "PID",                              # PID-0
+            "1",                                # PID-1
+            "",                                 # PID-2
+            faker.uuid4(),                      # PID-3
+            "",                                 # PID-4
+            generate_patient_name(),            # PID-5
+            faker.last_name(),                  # PID-6
+            generate_patient_birth_date(),      # PID-7
+            generate_gender(),                  # PID-8
+            "",                                 # PID-9
+            generate_patient_race(),            # PID-10
+            generate_patient_address(),         # PID-11
+            "",                                 # PID-12
+            generate_patient_phone_number(),    # PID-13
+            "",                                 # PID-14
+            "",                                 # PID-15
+            generate_patient_marital_status(),  # PID-16
+            "",                                 # PID-17
+            "",                                 # PID-18
+            generate_patient_ssn(),             # PID-19
         ]
     ).encode("utf-8")
+    # fmt: on
 
 
 def generate_evn_segment(trigger_event: str) -> bytes:
@@ -134,11 +104,15 @@ def generate_evn_segment(trigger_event: str) -> bytes:
     Returns: bytes - The EVN segment encoded in UTF-8.
     """
 
-    evn_0 = "EVN"
-    evn_1 = trigger_event
-    evn_2 = generate_random_date_time()
-
-    return SEPARATOR.join([evn_0, evn_1, evn_2]).encode("utf-8")
+    # fmt: off
+    return SEPARATOR.join(
+        [
+            "EVN",                          # EVN-0
+            trigger_event,                  # EVN-1
+            generate_random_date_time(),    # EVN-2
+        ]
+    ).encode("utf-8")
+    # fmt: on
 
 
 def generate_pv1_segment() -> bytes:
@@ -149,53 +123,33 @@ def generate_pv1_segment() -> bytes:
     Returns: bytes - The PV segment encoded in UTF-8.
     """
 
-    pv1_0 = "PV1"
-    pv1_1 = "0001"
-    pv1_2 = generate_patient_class()
-    pv1_3 = ""
-    pv1_4 = generate_admission_type()
-    pv1_5 = ""
-    pv1_6 = ""
-    pv1_7 = generate_doctor_name()
-    pv1_8 = generate_doctor_name()
-    pv1_9 = generate_doctor_name()
-    pv1_10 = generate_hospital_service()
-    pv1_ll = ""
-    pv1_12 = ""
-    pv1_13 = "R" if random.random() < 0.05 else ""  # 5% of admissions are readmissions
-    pv1_14 = generate_admit_source()
-    pv1_15 = generate_ambulatory_status()
-    pv1_16 = ""
-    pv1_17 = generate_doctor_name()
-    pv1_18_to_43 = "^" * 26
-    pv1_44 = generate_random_date_time()
-    pv1_45 = generate_random_date_time()
-
+    # fmt: off
     return SEPARATOR.join(
         [
-            pv1_0,
-            pv1_1,
-            pv1_2,
-            pv1_3,
-            pv1_4,
-            pv1_5,
-            pv1_6,
-            pv1_7,
-            pv1_8,
-            pv1_9,
-            pv1_10,
-            pv1_ll,
-            pv1_12,
-            pv1_13,
-            pv1_14,
-            pv1_15,
-            pv1_16,
-            pv1_17,
-            pv1_18_to_43,
-            pv1_44,
-            pv1_45,
+            "PV1",                                  # PV1-0
+            "0001",                                 # PV1-1
+            generate_patient_class(),               # PV1-2
+            "",                                     # PV1-3
+            generate_admission_type(),              # PV1-4
+            "",                                     # PV1-5
+            "",                                     # PV1-6
+            generate_doctor_name(),                 # PV1-7
+            generate_doctor_name(),                 # PV1-8
+            generate_doctor_name(),                 # PV1-9
+            generate_hospital_service(),            # PV1-10
+            "",                                     # PV1-11
+            "",                                     # PV1-12
+            "R" if random.random() < 0.05 else "",  # PV1-13
+            generate_admit_source(),                # PV1-14
+            generate_ambulatory_status(),           # PV1-15
+            "",                                     # PV1-16
+            generate_doctor_name(),                 # PV1-17
+            "^" * 26,                               # PV1-18 - PV1-43
+            generate_random_date_time(),            # PV1-44
+            generate_random_date_time(),            # PV1-45
         ]
     ).encode("utf-8")
+    # fmt: on
 
 
 def generate_segment(
