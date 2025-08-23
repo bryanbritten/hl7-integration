6a1b56d98e31bc8b18c15a71e9d4f71da55c6806
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-23 13:16:39 -0400

    adds more robust timing and coordination between minio, minio-client, and consumer services

--- docker/storage/minio-client/entrypoint.sh
+++ docker/storage/minio-client/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
+set -e
 
-sleep 5
+until mc alias set local "http://minio:9000" "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD" >/dev/null 2>&1; do
+  echo "Waiting for MinIO..."
+  sleep 1
+done
 
-mc alias set local http://minio:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
-mc mb --ignore-existing local/bronze
-mc anonymous set public local/bronze
-mc mb --ignore-existing local/silver
-mc anonymous set public local/silver
-mc mb --ignore-existing local/gold
-mc anonymous set public local/gold
-mc mb --ignore-existing local/deadletter
-mc anonymous set public local/deadletter
+echo "Successfully connected to MinIO..."
 
-# keep the container alive
-tail -f /dev/null
+for b in bronze silver gold deadletter; do
+  mc mb --ignore-existing "local/$b" || true
+  mc anonymous set public "local/$b" || true
+done
+
+exit 0
