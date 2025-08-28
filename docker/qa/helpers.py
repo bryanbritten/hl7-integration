40d2c532154cb82e17d2eca024a30fe9b03787bf
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-27 22:40:13 -0400

    implements basic functionality for performing QA checks

--- docker/qa/helpers.py
+++ docker/qa/helpers.py
@@ -1,18 +1,47 @@
-from datetime import datetime, timezone
+import json
 
-from hl7apy.exceptions import ParserError
-
-from common.helpers.hl7 import HL7Checker, HL7Parser, ValidationError
+from common.helpers.hl7 import HL7Checker, HL7Parser
 from common.helpers.kafka import to_header, write_to_topic
-from common.metrics import message_failures_total
 from common.registries import RULE_REGISTRY
+from common.rules.types import RuleResult
 
 
-def handle_error(**kwargs):
-    pass
+def handle_error(
+    e: Exception,
+    message: bytes,
+    message_type: str,
+    dlq_topic: str,
+) -> None:
+    headers = [
+        to_header("error.stage", "qa"),
+        to_header("error.message", f"Unexpected error occurred: {e}"),
+        to_header("hl7.message.type", message_type),
+        to_header("consumer.group.id", "hl7-qa"),
+    ]
+    write_to_topic(message, dlq_topic, headers)
 
 
-def record_validation_metrics(*args):
+def handle_failures(
+    failures: list[RuleResult], message: bytes, message_type: str, dlq_topic: str
+) -> None:
+    quality_issues = {failure.rule: failure.issues for failure in failures}
+    headers = [
+        to_header("hl7.message.type", message_type),
+        to_header("consumer.group.id", "hl7-qa"),
+        to_header("issues", json.dumps(quality_issues)),
+    ]
+    write_to_topic(message, dlq_topic, headers)
+
+
+def handle_success(message: bytes, message_type: str, write_topic: str) -> None:
+    headers = [
+        to_header("hl7.message.type", message_type),
+        to_header("consumer.group.id", "hl7-qa"),
+    ]
+    write_to_topic(message, write_topic, headers)
+
+
+def record_failures(*args):
     pass
 
 
@@ -22,61 +51,32 @@ def process_message(
     write_topic: str,
     dlq_topic: str,
 ) -> None:
-    timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S%f")
-    # initialize variables used in sending an ACK message
     parsed_message = None
-    message_control_id = None
-    # initialize validation metrics
-    missing_segments = []
-    invalid_segments = []
-    violating_segments = []
 
     try:
         parser = HL7Parser()
         parsed_message = parser.parse(message)
 
         checker = HL7Checker(parsed_message, message_type, RULE_REGISTRY)
+        results = checker.run_all_rules()
 
-    except ParserError as pe:
-        handle_error(
-            e=pe,
-            raw_message=message,
-            parsed_message=parsed_message,
-            dlq_topic=dlq_topic,
-        )
-        message_failures_total.labels(
-            type="parsing",
-            details="Failed to Parse Message",
-            stage="qa",
-            message_type=message_type,
-        ).inc()
-        return
-    except ValidationError as ve:
-        handle_error(
-            e=ve,
-            raw_message=message,
-            parsed_message=parsed_message,
-            dlq_topic=dlq_topic,
-        )
-        record_validation_metrics(
-            message_type,
-            message_control_id,
-            missing_segments,
-            invalid_segments,
-            violating_segments,
-        )
-        return
+        failures = [result for result in results if result.stamp == "Fail"]
+        if failures:
+            handle_failures(failures, message, message_type, dlq_topic)
+            return
+
+        handle_success(message, message_type, write_topic)
     except Exception as e:
         handle_error(
             e=e,
-            raw_message=message,
-            parsed_message=parsed_message,
+            message=message,
+            message_type=message_type,
             dlq_topic=dlq_topic,
         )
         return
 
     headers = [
         to_header("hl7.message.type", message_type),
-        to_header("consumer.group", "hl7-qa"),
+        to_header("consumer.group.id", "hl7-qa"),
     ]
     write_to_topic(message, write_topic, headers)
