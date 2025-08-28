99d18c53ef1b49ddb92e0bb7ef914b3f57b680fb
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-27 23:01:32 -0400

    replaces function names with actual functions in rule registry

--- common/registries/rules.py
+++ common/registries/rules.py
@@ -1,13 +1,19 @@
+from common.rules.segments.pid import (
+    family_name_is_not_placeholder,
+    given_name_is_not_placeholder,
+    patient_name_is_present,
+)
+
 RULE_REGISTRY = {
     "ADT_A01": [
-        "patient_name_is_present",
-        "given_name_is_not_placeholder",
-        "family_name_is_not_placeholder",
+        patient_name_is_present,
+        given_name_is_not_placeholder,
+        family_name_is_not_placeholder,
     ],
     "ADT_A03": [
-        "patient_name_is_present",
-        "given_name_is_not_placeholder",
-        "family_name_is_not_placeholder",
+        patient_name_is_present,
+        given_name_is_not_placeholder,
+        family_name_is_not_placeholder,
     ],
     "ORU_R01": [],
 }
