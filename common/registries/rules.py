6ff6809cac7e89bfdd886c6fc16ef905bce58b3b
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-27 22:55:41 -0400

    registers new QA rules for ADT_A01 and ADT_A03 messages

--- common/registries/rules.py
+++ common/registries/rules.py
@@ -1,5 +1,13 @@
 RULE_REGISTRY = {
-    "ADT_A01": [],
-    "ADT_A03": [],
+    "ADT_A01": [
+        "patient_name_is_present",
+        "given_name_is_not_placeholder",
+        "family_name_is_not_placeholder",
+    ],
+    "ADT_A03": [
+        "patient_name_is_present",
+        "given_name_is_not_placeholder",
+        "family_name_is_not_placeholder",
+    ],
     "ORU_R01": [],
 }
