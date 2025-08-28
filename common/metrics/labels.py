83eef9acc508621ea722af52d193eef5784a6881
Bryan Britten <85406498+bryanbritten@users.noreply.github.com>
2025-08-28 17:19:47 -0400

    creates a new labels submodule for metrics

---
+++ common/metrics/labels.py
@@ -0,0 +1,7 @@
+# Used in the Consumer service when validating a message on ingest
+REASON_MISSING_MSH10 = "Missing MSH-10 Field"
+REASON_MISSING_SEGMENTS = "Missing Required Segments"
+REASON_INVALID_SEGMENTS = "Contains Invalid Segments"
+REASON_INVALID_CARDINALITY = "Invalid Segment Cardinality"
+REASON_PARSING_ERROR = "Message Failed Parsing"
+REASON_UNKNOWN = "Unexpected Error Occurred"
--- common/metrics/utils.py
+++ common/metrics/utils.py
@@ -1,29 +1,17 @@
 from typing import Optional
 
 from common.metrics.counters import validation_error_total, validation_failed_total
-
-REASON_MISSING_MSH10 = "Missing MSH-10 Field"
-REASON_MISSING_SEGMENTS = "Missing Required Segments"
-REASON_INVALID_SEGMENTS = "Contains Invalid Segments"
-REASON_INVALID_CARDINALITY = "Invalid Segment Cardinality"
-REASON_PARSING_ERROR = "Message Failed Parsing"
-REASON_UNKNOWN = "Unexpected Error Occurred"
+from common.metrics.labels import REASON_MISSING_MSH10
 
 
 def record_validation_failures(
     message_type: str,
     message_control_id: Optional[str] = None,
-    failures: dict[str, str] = {},
+    failures: dict[str, bool] = {},
 ) -> None:
-    reasons = []
+    reasons = [k for k, v in failures.items() if v]
     if message_control_id is None:
         reasons.append(REASON_MISSING_MSH10)
-    if failures.get(REASON_MISSING_SEGMENTS):
-        reasons.append(REASON_MISSING_SEGMENTS)
-    if failures.get(REASON_INVALID_SEGMENTS):
-        reasons.append(REASON_INVALID_SEGMENTS)
-    if failures.get(REASON_INVALID_CARDINALITY):
-        reasons.append(REASON_INVALID_CARDINALITY)
 
     for reason in reasons:
         validation_error_total.labels(reason, "ingest", message_type).inc()
